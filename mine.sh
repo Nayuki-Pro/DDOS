f838a82ddb707241b58126ff67eb35281dee3438
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-05-27 09:02:03 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,4 @@
 wget https://github.com/NebuTech/NBMiner/releases/download/v42.2/NBMiner_42.2_Linux.tgz
 tar -zxvf NBMiner_42.2_Linux.tgz
-ls
+cd NBMiner_Linux
+./nbminer  -a ethash -o stratum+tcp://ethash.unmineable.com:3333 -u TRX:TBy3L5RcEF12sbdGm3S1YjhAvXsaPhzHEy.github#n598-vp1z -log
